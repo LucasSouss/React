0766e3b91f4b9d60e239906d5e192ec1089553e2
Lucas Sousa <lucassouss97@gmail.com>
2025-11-24 19:00:49 -0300

    git# Please enter the commit message for your changes. Lines starting hi

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,17 @@
-// Tasks.jsx
+
 
 import { ChevronRightIcon } from "lucide-react"; 
 
 function Tasks (props) {
   return (
-    // Aplica a classe para o <ul>
     <ul className="tasks-list"> 
       {props.tasks.map((task) => (
-        // Aplica a classe para o <li>
         <li key={task.id} className="task-item"> 
           
-          {/* O CSS Puro targeta este botão com ':first-child' */}
           <button> 
             {task.title}
           </button>
           
-          {/* O CSS Puro targeta este botão com ':last-child' */}
           <button title="Ver Detalhes">
             <ChevronRightIcon className="w-5 h-5" /> 
           </button>
