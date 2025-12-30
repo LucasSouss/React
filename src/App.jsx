c39ce8df291f13cf235b0d4f7ebd69ac14020326
Lucas Sousa <lucassouss97@gmail.com>
2025-12-30 18:05:42 -0300

    tenth-commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ function App() {
 
 
   //Chama API
-  useEffect(() => {
-    const fetchTasks = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {
-      method: 'GET',
-    }
-  )
-    const data = await response.json()
-    setTasks(data)
-    }
-    fetchTasks()
-},[])
+//   useEffect(() => {
+//     const fetchTasks = async () => {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {
+//       method: 'GET',
+//     }
+//   )
+//     const data = await response.json()
+//     setTasks(data)
+//     }
+//     fetchTasks()
+// },[])
 
 
   useEffect(() => {
--- src/Pages/TaskPages.jsx
+++ src/Pages/TaskPages.jsx
@@ -1,26 +1,28 @@
-
-import { ChevronLeftIcon } from "lucide-react"
-import { useNavigate, useSearchParams } from "react-router"
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router";
 
 export default function TaskPages() {
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams()
-    const title = searchParams.get('title')
-    const description = searchParams.get('description')
-    
-    return (
-        <div className="container-principal">
-            <div className="container-interno">
-                <div>
-                <button onClick={() => navigate(-1)}> {/* -1 faz com que a página volte a página anterior*/}
-                    <ChevronLeftIcon/>
-                </button>
-                <h1>Detalhes da Tarefa</h1>
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
 
-                </div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            </div>
+  return (
+    <div className="container-principal-pg">
+      <div className="container-interno-pg">
+        <div className="container-pg-header">
+          <button onClick={() => navigate(-1)}>
+            {" "}
+            {/* -1 faz com que a página volte a página anterior*/}
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="pg-header">Detalhes da Tarefa</h1>
         </div>
-    )
+        <div className="title-description">
+          <h2 className="pg-title">{title}</h2>
+          <p className="pg-description">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
