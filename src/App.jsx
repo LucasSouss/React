95ea362b6e7aed51f34e57715a318381a6f36c01
Lucas Sousa <lucassouss97@gmail.com>
2026-01-01 15:09:16 -0300

    tenth-commit-update

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,18 @@ function App() {
   )
 
 
-  //Chama API
-//   useEffect(() => {
-//     const fetchTasks = async () => {
-//       const response = await fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {
-//       method: 'GET',
-//     }
-//   )
-//     const data = await response.json()
-//     setTasks(data)
-//     }
-//     fetchTasks()
-// },[])
+  //Chama a API
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {
+      method: 'GET',
+    }
+  )
+    const data = await response.json()
+    setTasks(data)
+    }
+    fetchTasks()
+},[])
 
 
   useEffect(() => {
